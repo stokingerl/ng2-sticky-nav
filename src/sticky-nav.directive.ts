4a6a58fcddc5856ac07157d661f3f58fa2fcf832
Finnegan, Conor <Conor.Finnegan@fmr.com>
2019-09-09 12:13:13 -0400

    Added stickyEnabled Input for ng-sticky-nav to toggle sticky-ness based on provided logic.

--- src/sticky-nav.directive.ts
+++ src/sticky-nav.directive.ts
@@ -2,66 +2,71 @@ import { Directive, Input, Renderer, ElementRef, OnInit, OnDestroy } from '@angu
 import { fromEvent, Subscription } from 'rxjs';
 
 @Directive({
-    selector: '[ngStickyNav]'
+  selector: '[ngStickyNav]'
 })
 
 export class StickyNavDirective implements OnInit, OnDestroy {
-    private originalPosition: number;
-    private lastScroll: number = 0;
-    private isSticky: boolean = false;
-    private scrollSubscription: Subscription;
-    private wrapper: HTMLElement;
-    @Input('stickyClass') stickyClass: string;
+  private originalPosition: number;
+  private lastScroll: number = 0;
+  private isSticky: boolean = false;
+  private scrollSubscription: Subscription;
+  private wrapper: HTMLElement;
+  @Input('stickyClass') stickyClass: string;
+  @Input() stickyEnabled: boolean = true;
 
-    constructor(private elementRef: ElementRef, private renderer: Renderer) {
+  constructor(private elementRef: ElementRef, private renderer: Renderer) {
 
+  }
+
+  ngOnInit(): void {
+    this.scrollSubscription = fromEvent(window, 'scroll').subscribe(() => this.manageScrollEvent());
+  }
+
+  private manageScrollEvent(): void {
+    const scroll = window.pageYOffset;
+    if (
+      this.stickyEnabled &&
+      scroll > this.lastScroll &&
+      !this.isSticky &&
+      scroll >= this.elementRef.nativeElement.offsetTop
+    ) {
+      this.setSticky();
+    } else if (scroll < this.lastScroll && this.isSticky && scroll <= this.originalPosition) {
+      this.unsetSticky();
     }
+    this.lastScroll = scroll;
+  }
 
-    ngOnInit(): void {
-        this.scrollSubscription = fromEvent(window, 'scroll').subscribe(() => this.manageScrollEvent());
+  private setSticky(): void {
+    this.isSticky = true;
+    this.originalPosition = this.elementRef.nativeElement.offsetTop;
+    this.wrapper = this.elementRef.nativeElement.cloneNode(true);
+    this.setStyle('position', 'fixed');
+    this.setStyle('top', '0');
+    this.setClass(true);
+    this.renderer.setElementStyle(this.wrapper, 'visibility', 'hidden');
+    this.elementRef.nativeElement.parentElement.insertBefore(this.wrapper, this.elementRef.nativeElement);
+  }
+
+  private unsetSticky(): void {
+    this.isSticky = false;
+    this.originalPosition = 0;
+    this.elementRef.nativeElement.parentElement.removeChild(this.wrapper);
+    this.setStyle('position', 'static');
+    this.setClass(false);
+  }
+
+  private setStyle(key: string, value: string): void {
+    this.renderer.setElementStyle(this.elementRef.nativeElement, key, value);
+  }
+
+  private setClass(add: boolean): void {
+    this.renderer.setElementClass(this.elementRef.nativeElement, this.stickyClass, add);
+  }
+
+  ngOnDestroy() {
+    if (this.scrollSubscription) {
+      this.scrollSubscription.unsubscribe();
     }
-
-    private manageScrollEvent(): void {
-        const scroll = window.pageYOffset;
-
-        if (scroll > this.lastScroll && !this.isSticky && scroll >= this.elementRef.nativeElement.offsetTop) {
-            this.setSticky();
-        } else if (scroll < this.lastScroll && this.isSticky && scroll <= this.originalPosition) {
-            this.unsetSticky();
-        }
-        this.lastScroll = scroll;
-    }
-
-    private setSticky(): void {
-        this.isSticky = true;
-        this.originalPosition = this.elementRef.nativeElement.offsetTop;
-        this.wrapper = this.elementRef.nativeElement.cloneNode(true);
-        this.setStyle('position', 'fixed');
-        this.setStyle('top', '0');
-        this.setClass(true);
-        this.renderer.setElementStyle(this.wrapper, 'visibility', 'hidden');
-        this.elementRef.nativeElement.parentElement.insertBefore(this.wrapper, this.elementRef.nativeElement);
-    }
-
-    private unsetSticky(): void {
-        this.isSticky = false;
-        this.originalPosition = 0;
-        this.elementRef.nativeElement.parentElement.removeChild(this.wrapper);
-        this.setStyle('position', 'static');
-        this.setClass(false);
-    }
-
-    private setStyle(key: string, value: string): void {
-        this.renderer.setElementStyle(this.elementRef.nativeElement, key, value);
-    }
-
-    private setClass(add: boolean): void {
-        this.renderer.setElementClass(this.elementRef.nativeElement, this.stickyClass, add);
-    }
-
-    ngOnDestroy() {
-        if (this.scrollSubscription) {
-            this.scrollSubscription.unsubscribe();
-        }
-    }
+  }
 }
